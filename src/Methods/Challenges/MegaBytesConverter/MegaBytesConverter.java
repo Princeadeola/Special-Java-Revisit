2b3d0c09b6a52fc40cf4b4e8347cf510384ca0a7
Adesipe Adeola <56828114+Princeadeola@users.noreply.github.com>
2022-06-23 08:13:24 -0700

    commit

--- src/Methods/Challenges/MegaBytesConverter/MegaBytesConverter.java
+++ src/Methods/Challenges/MegaBytesConverter/MegaBytesConverter.java
@@ -2,4 +2,25 @@ package Methods.Challenges.MegaBytesConverter;
 
 public class MegaBytesConverter {
 
+    public static void printMegaBytesAndKiloBytes(int kilobytes){
+        //1MB = 1024 KB
+        //2500KB = 2MB AND 452KB
+
+        if (kilobytes < 0){
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        int megabyte = (kilobytes / 1024);
+        int remainingKilobytes = (kilobytes % 1024);
+
+        System.out.println(kilobytes + " KB = " + megabyte +" MG AND " + remainingKilobytes + " KB");
+    }
+
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(2500);
+        printMegaBytesAndKiloBytes(-1024);
+        printMegaBytesAndKiloBytes(5000);
+    }
+
 }
