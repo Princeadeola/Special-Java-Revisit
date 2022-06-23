648314cb87170dbcdc896b0f183c5fae7f7be5d4
Adesipe Adeola <56828114+Princeadeola@users.noreply.github.com>
2022-06-23 09:07:16 -0700

    commit

---
+++ src/Methods/Challenges/EqualSumChecker/EqualSumChecker.java
@@ -0,0 +1,13 @@
+package Methods.Challenges.EqualSumChecker;
+
+public class EqualSumChecker {
+
+    public static boolean sumNumber(int firstNumber, int secondNumber, int thirdNumber) {
+        if (firstNumber + secondNumber == thirdNumber) {
+            return true;
+        }else   {
+            return false;
+        }
+    }
+
+}
