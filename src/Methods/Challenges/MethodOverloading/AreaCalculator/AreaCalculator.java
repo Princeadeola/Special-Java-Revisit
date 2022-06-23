67dac12aabf3230de32e7514d432fd2e99a9ea80
Adesipe Adeola <56828114+Princeadeola@users.noreply.github.com>
2022-06-23 13:13:32 -0700

    commit

--- src/Methods/Challenges/MethodOverloading/AreaCalculator/AreaCalculator.java
+++ src/Methods/Challenges/MethodOverloading/AreaCalculator/AreaCalculator.java
@@ -1,4 +1,13 @@
 package Methods.Challenges.MethodOverloading.AreaCalculator;
 
 public class AreaCalculator {
+
+    public static double area (double radius) {
+        return (radius < 0) ? -1 : radius * radius * Math.PI;
+    }
+
+    public static double area (double x, double y) {
+        return (x < 0 || y < 0) ? -1 : x * y;
+    }
+
 }
