5b4493cde111cd5a7d0bd13083ad73e133f61b86
princeadeola <princeadeola234@gmail.com>
2022-07-12 07:37:49 +0100

    committed

--- src/Inheritance/Challenges/Cylinder/Circle.java
+++ src/Inheritance/Challenges/Cylinder/Circle.java
@@ -3,4 +3,18 @@ package Inheritance.Challenges.Cylinder;
 public class Circle {
     private double radius;
 
+    public Circle(double radius) {
+        if (radius < 0){
+            radius = 0;
+        }
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea(){
+        return (radius * radius * Math.PI);
+    }
 }
