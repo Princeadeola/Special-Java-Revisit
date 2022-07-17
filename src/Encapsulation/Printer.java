37e9d497f229e89e70fa19e5956b3e39db50f494
princeadeola <princeadeola234@gmail.com>
2022-07-17 20:47:10 +0100

    committed

---
+++ src/Encapsulation/Printer.java
@@ -0,0 +1,12 @@
+package Encapsulation;
+
+public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        this.tonerLevel = tonerLevel;
+        this.duplex = duplex;
+    }
+}
