d5f4dcae4f6394bd81bf27b1de8366810fd15eb8
princeadeola <princeadeola234@gmail.com>
2022-07-17 21:04:28 +0100

    committed

--- src/Encapsulation/Printer.java
+++ src/Encapsulation/Printer.java
@@ -6,7 +6,36 @@ public class Printer {
     private boolean duplex;
 
     public Printer(int tonerLevel, boolean duplex) {
-        this.tonerLevel = tonerLevel;
+        if (tonerLevel > -1 && tonerLevel <= 100){
+            this.tonerLevel = tonerLevel;
+        }else {
+            this.tonerLevel = -1;
+        }
         this.duplex = duplex;
+        this.pagesPrinted = 0;
+    }
+
+    public int addToner(int tonerAmount){
+        if (tonerAmount > 0 && tonerAmount <= 100) {
+            if ((tonerLevel+tonerAmount) > 100){
+                return -1;
+            }
+            return (tonerLevel + tonerAmount);
+        }
+        return -1;
+    }
+
+    public int printPages(int pages){
+        int pagesToPrint = pages;
+        if (duplex){
+            pagesToPrint = ((pagesToPrint/2) + (pagesToPrint % 2));
+            pagesPrinted = pagesToPrint;
+        }
+
+        return pagesToPrint;
+    }
+
+    public int getPagesPrinted(){
+        return pagesPrinted;
     }
 }
