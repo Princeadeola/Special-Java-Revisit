bb3aa2c5040e593428ae8d8d5a42e9d04313ec47
princeadeola <princeadeola234@gmail.com>
2022-07-17 21:18:36 +0100

    committed

--- src/Encapsulation/Printer.java
+++ src/Encapsulation/Printer.java
@@ -1,41 +1,53 @@
 package Encapsulation;
 
 public class Printer {
+
     private int tonerLevel;
-    private int pagesPrinted;
+    private  int pagesPrinted;
     private boolean duplex;
 
     public Printer(int tonerLevel, boolean duplex) {
-        if (tonerLevel > -1 && tonerLevel <= 100){
+        if( tonerLevel >-1 || tonerLevel <= 100){
             this.tonerLevel = tonerLevel;
         }else {
             this.tonerLevel = -1;
         }
+
         this.duplex = duplex;
-        this.pagesPrinted = 0;
+        this.pagesPrinted =0;
     }
 
     public int addToner(int tonerAmount){
-        if (tonerAmount > 0 && tonerAmount <= 100) {
-            if ((tonerLevel+tonerAmount) > 100){
+        if(tonerAmount >0 && tonerAmount<100){
+            if((tonerAmount+ this.tonerLevel)> 100){
                 return -1;
+            }else {
+                this.tonerLevel = (this.tonerLevel + tonerAmount);
             }
-            return (tonerLevel + tonerAmount);
+
+        }else {
+            return -1;
         }
-        return -1;
+
+        return this.tonerLevel;
     }
 
-    public int printPages(int pages){
+    public int printPages (int pages){    // need to include isduplex here,
+
         int pagesToPrint = pages;
-        if (duplex){
-            pagesToPrint = ((pagesToPrint/2) + (pagesToPrint % 2));
-            pagesPrinted = pagesToPrint;
+
+
+        if (this.duplex){
+            pagesToPrint = (pages / 2) + (pages % 2);
+            System.out.println("printing in duplex mode ");
         }
 
+        this.pagesPrinted = this.pagesPrinted + pagesToPrint;
         return pagesToPrint;
+
     }
 
-    public int getPagesPrinted(){
+    public int getPagesPrinted() {
         return pagesPrinted;
     }
 }
