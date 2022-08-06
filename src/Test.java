3e87319fd487fbd7894bd30f9bf07b275b163511
Adesipe Adeola <princeadeola234@gmail.com>
2022-08-05 18:32:36 -0700

    committed

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter radius: ");
+        int radius = input.nextInt();
 
+        final double PI = 3.14;
+        double area = 2*PI*(radius * radius);
+        System.out.println("Area = " + area);
     }
 }
