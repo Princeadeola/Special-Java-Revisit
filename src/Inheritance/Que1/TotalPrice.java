ccc2cb387c2c67d6f4f45a9fc305c31f84e2b7d2
Adesipe Adeola <princeadeola234@gmail.com>
2022-10-10 13:42:33 -0700

    committed

---
+++ src/Inheritance/Que1/TotalPrice.java
@@ -0,0 +1,21 @@
+package Inheritance.Que1;
+
+public class TotalPrice {
+    //This class displays the total prices, it is broken down into
+
+    //<========== Base fare (the primary cost of the airplane boarding) ===========>
+
+    //<========== Taxes (The taxes is made up of ) ================>
+    //Airport service charge
+
+    //security charges
+
+    //sales tax(Domestic)
+
+    //s2
+
+    // <======== Surcharges =========>
+
+
+    //<========= Total ===============>
+}
