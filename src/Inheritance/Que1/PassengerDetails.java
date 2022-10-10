9c83b9c2d0bf722ac8ab345529d92af502a71c68
Adesipe Adeola <princeadeola234@gmail.com>
2022-10-10 13:42:24 -0700

    committed

---
+++ src/Inheritance/Que1/PassengerDetails.java
@@ -0,0 +1,14 @@
+package Inheritance.Que1;
+
+//This is the passenger details class that displays
+//The travel route, Seat location and the e-ticket Number
+public class PassengerDetails {
+    public static void main(String[] args) {
+        System.out.println("Welcome back");
+
+        System.out.println("Flight from Lagos to London");
+        System.out.println("seat number: Seat 30L");
+        System.out.println("e-Ticket Number: 0223122332");
+    }
+
+}
