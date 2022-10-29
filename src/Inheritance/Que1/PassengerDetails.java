217da19a2f9b3081eaa1b5cebdcb26f3cc3113b9
Adesipe Adeola <princeadeola234@gmail.com>
2022-10-28 20:40:27 -0700

    committed

--- src/Inheritance/Que1/PassengerDetails.java
+++ src/Inheritance/Que1/PassengerDetails.java
@@ -4,9 +4,11 @@ package Inheritance.Que1;
 //The travel route, Seat location and the e-ticket Number
 public class PassengerDetails {
     public static void main(String[] args) {
-        System.out.println("Welcome back");
+        Smartwave smartwave = new Smartwave();
 
-        System.out.println("Flight from Lagos to London");
+        System.out.println("Welcome back " + smartwave.name);
+
+        System.out.println("Flight from " + smartwave.departure + " to " + smartwave.destination);
         System.out.println("seat number: Seat 30L");
         System.out.println("e-Ticket Number: 0223122332");
     }
