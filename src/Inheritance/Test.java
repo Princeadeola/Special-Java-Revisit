f33fd96911817f6fbeb658c43a690ab359698617
Adesipe Adeola <princeadeola234@gmail.com>
2022-10-28 20:40:52 -0700

    committed

--- src/Inheritance/Test.java
+++ src/Inheritance/Test.java
@@ -1,4 +1,11 @@
 package Inheritance;
 
+import java.util.Random;
+
 public class Test {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int numberOfGuess = rand.nextInt(50);
+        System.out.println(numberOfGuess);
+    }
 }
