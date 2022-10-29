7e5a06c4f776d845fb834eae8a64b7d2da7428e8
Adesipe Adeola <princeadeola234@gmail.com>
2022-10-28 21:03:49 -0700

    committed

--- src/Debugging/Main.java
+++ src/Debugging/Main.java
@@ -2,6 +2,13 @@ package Debugging;
 
 public class Main {
     public static void main(String[] args) {
+        StringUtilities utilities = new StringUtilities();
+        StringBuilder stringBuilder = new StringBuilder();
 
+        while (stringBuilder.length() < 10){
+            utilities.addChar(stringBuilder, 'a');
+        }
+
+        System.out.println(stringBuilder);
     }
 }
